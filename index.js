c6072316357cc1a0372d02744feaf9498f9a8bb9
Saravana kumar M <98660788+msk-saravana@users.noreply.github.com>
2024-01-17 13:03:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//Digital clock program
+
+function updateClock(){
+    let now=new Date();
+    let hours=now.getHours();
+    const meridium=hours>=12 ?"PM":"AM";
+    hours=hours%12 || 12;
+    hours=hours.toString().padStart(2,0);
+    let minutes=now.getMinutes().toString().padStart(2,0);
+    let seconds=now.getSeconds().toString().padStart(2,0);
+    let timeString=`${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent=timeString;
+}
+updateClock();
+setInterval(updateClock,1000);
